5007772f1a5ebb6d10fd7825a46c9e36cc7b2829
夕輝 豊田 <K022G0030@g.neec.ac.jp>
2022-10-26 11:26:16 +0900

    third branchに修正

--- Git_Test/Program.vb
+++ Git_Test/Program.vb
@@ -4,5 +4,6 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello GitHub!\n")
         Console.WriteLine("Hello New Branch\n")
+        Console.WriteLine("third branch\n")
     End Sub
 End Module
