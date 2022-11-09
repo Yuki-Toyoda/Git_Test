f981771ac4ba96a9ffe98b7c4fb23826a76b35f4
夕輝 豊田 <K022G0030@g.neec.ac.jp>
2022-11-09 11:46:10 +0900

    New branch changed

--- Git_Test/Program.vb
+++ Git_Test/Program.vb
@@ -3,6 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello GitHub!\n")
-        Console.WriteLine("GitHub lone changed\n")
+        Console.WriteLine("New branch changed\n")
     End Sub
 End Module
